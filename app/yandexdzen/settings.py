86ba4bc60e5a5a0a31073876ecc0a7655f505990
Sultan Usmanov <susmanov@hostkey.com>
2023-01-21 20:19:14 +0600

    settings.py changed

--- app/yandexdzen/settings.py
+++ app/yandexdzen/settings.py
@@ -141,5 +141,3 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ]}
-
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS').split(',')
