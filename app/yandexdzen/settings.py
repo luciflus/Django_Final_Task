abb16ac59baf59bd7ea1f5035d156d7db228129c
Sultan Usmanov <susmanov@hostkey.com>
2023-01-21 20:23:47 +0600

    settings.py '/static/'

--- app/yandexdzen/settings.py
+++ app/yandexdzen/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 STATIC_ROOT = [os.path.join(BASE_DIR, 'templates', 'static')]
 
 # Quick-start development settings - unsuitable for production
