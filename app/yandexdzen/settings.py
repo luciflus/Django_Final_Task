d87b9b2660325baddaa56b393a0022790eda2af8
Sultan Usmanov <susmanov@hostkey.com>
2023-01-21 20:28:56 +0600

    settings.py  staticfiles

--- app/yandexdzen/settings.py
+++ app/yandexdzen/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [os.path.join(BASE_DIR, 'templates', 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
