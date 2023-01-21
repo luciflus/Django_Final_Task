3215bbe8d0d3249a9f465c3d433959e76996edaa
Sultan Usmanov <susmanov@hostkey.com>
2023-01-21 20:37:25 +0600

    debug fixed

--- app/yandexdzen/settings.py
+++ app/yandexdzen/settings.py
@@ -26,7 +26,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get('DEBUG', default=0))
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
